aac61be535edca56eb6feb2ba958926ddfe16ad5
vietnguyen-thrivent <102984405+vietnguyen-thrivent@users.noreply.github.com>
2022-06-22 12:23:13 -0400

    build style dictionary

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 22 Jun 2022 16:22:31 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
